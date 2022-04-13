e67d6efe5315bd6f1eb69cbe93bc2cdaab437e99
Gaezort <gaezort@gmail.com>
2022-04-13 00:02:44 -0300

    first commit

---
+++ javascript/funciones/ejercicios/ejercicio1.js
@@ -0,0 +1,27 @@
+//Hacer que la funcion multiplicador() reciba 2 parametros y devuelva la multiplicacion de ambos
+/*Respuesta linea 23
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
+
+function multiplicador(x,y){
+    return x,y
+}
+
+console.log(multiplicador(2,5));
