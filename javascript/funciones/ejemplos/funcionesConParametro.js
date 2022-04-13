e308832885de41fa784c9d1b306ee125dcb2d3e8
Gaezort <gaezort@gmail.com>
2022-04-12 23:57:40 -0300

    first commit

---
+++ javascript/funciones/ejemplos/funcionesConParametro.js
@@ -0,0 +1,6 @@
+//Una funcion que reciba parametros y devuelva un valor (la multiplicacion de ambos parametros)
+function funcionConParametros(x,y){
+return x*y;
+}
+
+console.log(funcionConParametros(2,5));
